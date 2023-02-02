9dc66fe350fce3987f7950e17307df371ceda6cd
Eric Hamilton <ericjean873@gmail.com>
2023-02-02 00:33:25 -0500

    changed regex to be more specific and filter out unwanted data.

--- src/import.py
+++ src/import.py
@@ -21,7 +21,7 @@ def parse_urls(url: str) -> List[str]:
 def main():
     url = "https://www.smogon.com/stats/"
     month_links = parse_urls(url)
-    gen_7_data = []
+    total_rows = 0
     for month in month_links:
         # the chaos subfolder contains the full json data that we need. the txt files just mimic
         # what the json folder has; and is easier to process.
@@ -30,7 +30,10 @@ def main():
         chaos_url = f"{url}{month}chaos/"
         for data_file in parse_urls(chaos_url):
             data_url = f"{chaos_url}{data_file}"
-            if re.match(r"^gen7.*\.json$", data_file):
+            if re.match(
+                r"^gen7(doubles)?(ou|ubers|anythinggoes|vgc\d{4})-\d+\.json$",
+                data_file,
+            ):
                 # for now i'm just getting the json response for gen 7 data. what we can do from here is:
                 # - create a csv which we individually process into data
                 # - store this into a database, which we also construct in this python file
@@ -43,8 +46,8 @@ def main():
                     for k in data[name].keys():
                         trans_data[len(trans_data) - 1][k] = data[name][k]
                 data_df = pd.json_normalize(trans_data, max_level=0)
-                gen_7_data.append(data_df)
-    print(gen_7_data)
+                total_rows += len(data_df.index)
+    print(total_rows)
 
 
 if __name__ == "__main__":
